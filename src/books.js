6450a7c82eb1e19958042cd22757a6310ae5c730
PaulinaST <paustrzy@gmail.com>
2023-04-14 13:17:44 +0200

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './img/book1.jpg'
+import img2 from './img/book2.jpg'
+import img3 from './img/book3.jpg'
+
+export const books = [
+	{
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		image: img1,
+		id: 1,
+	},
+	{
+		title: 'Outlive: The Science and Art of Longevity',
+		author: 'Peter Attia MD',
+		image: img2,
+		id: 2,
+	},
+	{
+		title: 'The 48 Laws of Power',
+		author: 'Robert Greene',
+		image: img3,
+		id: 3,
+	},
+]
